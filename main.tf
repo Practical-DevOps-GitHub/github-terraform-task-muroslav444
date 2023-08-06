f91d71d90bb5d38cb169494d5346c508669d94b0
Myroslav Vsiakyi <74293974+muroslav444@users.noreply.github.com>
2023-08-06 09:01:32 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 provider "github" {
-  token = "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" 
+  token = "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"  
 }
 
 resource "github_repository" "repo" {
@@ -23,18 +23,23 @@ resource "github_branch_default" "default" {
   branch     = github_branch.develop.branch
 }
 
-resource "github_branch_protection" "main" {
+resource "github_branch_protection_rule" "main" {
   repository = github_repository.repo.name
-  branch     = github_branch.main.branch
-  enforce_admins = true
+  pattern    = "main"
+  enforcement_level = "enforce"
+  require_code_owner_reviews = false
 }
 
-resource "github_branch_protection" "develop" {
+resource "github_branch_protection_rule" "develop" {
   repository = github_repository.repo.name
-  branch     = github_branch.develop.branch
-  require_pull_request_reviews {
-    dismiss_stale_reviews     = true
-    require_code_owner_reviews = false
+  pattern    = "develop"
+  enforcement_level = "enforce"
+  require_code_owner_reviews = false
+
+  required_pull_request_reviews {
+    dismissal_restrictions {
+      users = ["*"]
+    }
     required_approving_review_count = 2
   }
 }
@@ -45,20 +50,27 @@ resource "github_repository_collaborator" "softservedata" {
   permission = "pull"
 }
 
-resource "github_file" "pull_request_template" {
+resource "github_repository_file" "pull_request_template" {
   repository = github_repository.repo.name
   file_path  = ".github/pull_request_template.md"
-  content    = <<EOF
-Describe your changes
+  content    = <<-EOF
+    Describe your changes
 
-Issue ticket number and link
+    Issue ticket number and link
 
-Checklist before requesting a review
-- [ ] I have performed a self-review of my code
-- [ ] If it is a core feature, I have added thorough tests
-- [ ] Do we need to implement analytics?
-- [ ] Will this be part of a product update? If yes, please write one phrase about this update
-EOF
+    Checklist before requesting a review
+    - [ ] I have performed a self-review of my code
+    - [ ] If it is a core feature, I have added thorough tests
+    - [ ] Do we need to implement analytics?
+    - [ ] Will this be part of a product update? If yes, please write one phrase about this update
+  EOF
+}
+
+resource "github_deploy_key" "deploy_key" {
+  repository = github_repository.repo.name
+  title      = "DEPLOY_KEY"
+  key        = var.ssh_public_key  # Provide your public SSH key here
+  read_only  = true
 }
 
 resource "github_actions_secret" "pat_secret" {
@@ -72,7 +84,8 @@ resource "github_repository_webhook" "discord_webhook" {
   name           = "discord"
   active         = true
   events         = ["pull_request"]
-  configuration = {
+  
+  configuration {
     url          = "https://discord.com/api/webhooks/1131582221589942342/QPu0CLB0XO-QMDI31JyHHX72YuGoILhZK9Z_aJTpKSpyIi0eDzMYPkSQ0FGHM0arwv2_"  # Replace with your Discord webhook URL
     content_type = "json"
   }
