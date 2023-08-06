94f83b97f85486f830bba092d8e7503eafff2e1a
Myroslav Vsiakyi <74293974+muroslav444@users.noreply.github.com>
2023-08-06 08:39:43 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,96 +1,91 @@
-terraform {
-  required_providers {
-    github = {
-      source = "hashicorp/github"
-    }
-  }
-}
-
 provider "github" {
-  organization = "Practical-DevOps-GitHub/github-terraform-task-muroslav444"
   token = var.github_token
 }
 
 resource "github_repository" "repo" {
-  name = "github-terraform-task-muroslav444"
-  description = "This is my GitHub repository."
-  visibility = "private"
+  name        = "github-terraform-task-muroslav444"  
+  description = "Your repository description"  
+  visibility  = "private"  
+}
+
+resource "github_branch" "develop" {
+  repository = github_repository.repo.name
+  branch     = "develop"
+}
+
+resource "github_branch_default" "develop" {
+  repository = github_repository.repo.name
+  branch     = github_branch.develop.branch
 }
 
 resource "github_branch_protection" "main" {
   repository = github_repository.repo.name
-  branch = "main"
+  branch     = "main"
   enforce_admins = true
-  require_pull_request_reviews = true
-  required_review_count = 2
 }
 
 resource "github_branch_protection" "develop" {
   repository = github_repository.repo.name
-  branch = "develop"
-  enforce_admins = true
-  require_pull_request_reviews = true
-  required_review_count = 2
+  branch     = github_branch.develop.branch
+  require_pull_request_reviews {
+    dismiss_stale_reviews     = true
+    require_code_owner_reviews = false
+    required_approving_review_count = 2
+  }
 }
 
-resource "github_collaborator" "softservedata" {
+resource "github_repository_collaborator" "softservedata" {
   repository = github_repository.repo.name
-  user = "softservedata"
-  permissions = ["pull", "push", "admin"]
+  username   = "softservedata"
+  permission = "pull"
 }
 
-resource "github_pull_request_template" "pull_request_template" {
+resource "github_file" "pull_request_template" {
   repository = github_repository.repo.name
-  path = ".github/pull_request_template.md"
-  content = <<EOF
-# Pull Request Template
+  file_path  = ".github/pull_request_template.md"
+  content    = <<EOF
+Describe your changes
 
-## Describe your changes
+Issue ticket number and link
 
-Please describe your changes in detail.
-
-## Issue ticket number and link
-
-Please provide the issue ticket number and link.
-
-## Checklist before requesting a review
-
-* I have performed a self-review of my code.
-* If it is a core feature, I have added thorough tests.
-* Do we need to implement analytics?
-* Will this be part of a product update? If yes, please write one phrase about this update.
+Checklist before requesting a review
+- [ ] I have performed a self-review of my code
+- [ ] If it is a core feature, I have added thorough tests
+- [ ] Do we need to implement analytics?
+- [ ] Will this be part of a product update? If yes, please write one phrase about this update
 EOF
 }
 
 resource "github_deploy_key" "deploy_key" {
   repository = github_repository.repo.name
-  title = "DEPLOY_KEY"
-  key = file("~/.ssh/id_rsa.pub")
+  title      = "DEPLOY_KEY"
+  key        = var.ssh_public_key  # Provide your public SSH key here
+  read_only  = true
 }
 
-resource "github_integration" "discord" {
+resource "github_actions_secret" "pat_secret" {
   repository = github_repository.repo.name
-  integration_id = "discord"
+  secret_name = "PAT"
+  plaintext_value = var.github_token
+}
+
+# Replace the webhook URL with your Discord webhook URL
+resource "github_repository_webhook" "discord_webhook" {
+  repository     = github_repository.repo.name
+  name           = "discord"
+  active         = true
+  events         = ["pull_request"]
   configuration = {
-    webhook_url = "https://discord.com/api/webhooks/1131582221589942342/QPu0CLB0XO-QMDI31JyHHX72YuGoILhZK9Z_aJTpKSpyIi0eDzMYPkSQ0FGHM0arwv2_"
-    events = ["pull_request"]
+    url          = "https://discord.com/api/webhooks/1131582221589942342/QPu0CLB0XO-QMDI31JyHHX72YuGoILhZK9Z_aJTpKSpyIi0eDzMYPkSQ0FGHM0arwv2_"
+    content_type = "json"
   }
 }
 
-resource "github_actions_secret" "pat" {
-  repository = github_repository.repo.name
-  secret_name = "PAT"
-  secret_value = var.pat
-}
-
+# Variables
 variable "github_token" {
-  type = string
+  description = "GitHub Personal Access Token with Full control of private repositories, Full control of orgs and teams, and read and write org projects"
 }
 
-variable "pat" {
-  type = string
-}
-
-output "terraform_code" {
-  value = file("main.tf")
+variable "ssh_public_key" {
+  description = "SSH public key to be used for the DEPLOY_KEY"
 }
